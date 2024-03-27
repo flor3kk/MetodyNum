2a79528c1d8f6e8413c8a1165f4ef004977c7125
flor3kk <127989634+flor3kk@users.noreply.github.com>
2024-03-27 09:55:15 +0100

    Update STYCZNE.py

--- STYCZNE.py
+++ STYCZNE.py
@@ -1,17 +1,24 @@
 # METODA STYCZNYCH
 import math
 
+# IMPORT SYMPY!!!!!!!!! DO ZROBIENIA
 
 def funkcja(x):
-    return math.sin(x) - (1 / 2) * x
-
+    #return math.sin(x) - (1 / 2) * x
+    return x ** 3 + x ** 2 - 3 * x - 3
 
 def fprim(x):
-    return math.cos(x) - 1 / 2
+    #return math.cos(x) - 1 / 2
+    return 3 * x ** 2 + 2 *x - 3
+
+def fprim2(x):
+    #return -math.sin(x)
+    return 6 * x + 2
 
 def x_i(b):
     x = b - (funkcja(b) / fprim(b))
     return x
+
 
 def warunekCauchyego(a, b):
     if funkcja(a) * funkcja(b) < 0:
@@ -21,10 +28,16 @@ def warunekCauchyego(a, b):
 
 
 if __name__ == '__main__':
-    E = 0.001
+    E = 0.00001
     c = 1  # losowa wartość większa od E
-    a = math.pi / 2
-    b = math.pi
+    a = 1
+    b = 2
+
+    if funkcja(a) * fprim2(a) > 0:
+        x = a
+    elif funkcja(b) * fprim2(b) < 0:
+        x = b
+
 
     i = 0  # obroty
     while (abs(c) > E and warunekCauchyego(a, b)):
@@ -33,7 +46,7 @@ if __name__ == '__main__':
         x = x_i(b)
         c = funkcja(x)
 
-        print("lewy przedzial: ", a, "\t\t prawy przedzial: ", b, "\t\tx: ", x, "\tf(x) = ", c," ---- ITERACJE: ", i)
+        print("lewy przedzial: ", a, "\t\t prawy przedzial: ", b, "\t\tx: ", x, "\tf(x) = ", c, " ---- ITERACJE: ", i)
 
         if (warunekCauchyego(a, x)):
             b = x
